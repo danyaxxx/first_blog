a79beda6a24c500eeb89afaab9de11cfc50de3e4
Daniil <danya.trishchanovskiy.01@mail.ru>
2020-05-29 19:11:15 +0300

    Start

---
+++ models/articles.php
@@ -0,0 +1,23 @@
+<?php
+
+function artcles_all() {
+    
+}
+
+function artcles_get($id) {
+    
+}
+
+function artcles_new($title, $date, $content) {
+    
+}
+
+function artcles_edit($id, $title, $date, $content) {
+    
+}
+
+function artcles_delite($id) {
+    
+}
+
+?>
