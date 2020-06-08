1eeed5d7f2d599349c3f7d9a474f4f4fbe5a08eb
Daniil <danya.trishchanovskiy.01@mail.ru>
2020-06-08 18:35:37 +0300

    Output of articles from the database

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'daniil');
+define('MYSQL_PASSWORD', '666a74810');
+define('MYSQL_DB', 'blog');
+
+function db_connect() {
+    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
+        or die("Error: ".mysqli_error($link));
+
+    if(!mysqli_set_charset($link, "utf8")) {
+        printf("Error: ".mysqli_error($link));
+    }
+
+    return $link;
+}
+
+?>
