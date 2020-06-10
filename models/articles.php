e5b51e96ccf94b71c27239cd0db818ea5e01cfe3
Daniil <danya.trishchanovskiy.01@mail.ru>
2020-06-10 14:20:27 +0300

    Deleting articles and shortening the inital text

--- models/articles.php
+++ models/articles.php
@@ -78,7 +78,25 @@ function articles_edit($link, $id, $title, $date, $content) {
     return mysqli_affected_rows($link);
 }
 
-function articles_delete($id) {
+function articles_delete($link, $id) {
+    $id = (int)$id;
+
+    // Проверка
+    if ($id == 0)
+        return false;
+
+    // Запрос
+    $query = sprintf("DELETE FROM articles WHERE id='%d'", $id);
+    $result = mysqli_query($link, $query);
+
+    if (!$result)
+        die(mysqli_error($link));
+
+    return mysqli_affected_rows($link);
+}
+
+function articles_intro($text, $len = 500) {
+    return mb_substr($text, 0, $len);
 }
 
 ?>
